d03e7ff8c96631ce297fcee9be1a00eaa6273c8c
Erik Ashley <eashley415@gmail.com>
2014-04-04 19:32:12 -0400

    update2

--- index.php
+++ index.php
@@ -1,66 +1,58 @@
 <!DOCTYPE html>
 <html>
 
-	<head>
-		<meta charset="utf-8"/>
-		<title>Audio and Sound Design for Video Games</title>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Audio and Sound Design for Video Games</title>
 
-		<link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:700" rel="stylesheet" />
+        <link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:700" rel="stylesheet" />
 
-		<link href="style.css" rel="stylesheet" />
+        <link href="style.css" rel="stylesheet" />
 
-	</head>
+    </head>
+        <body class="title_page">
+            <div id="main">
+                <h1>A Step Closer to Audio Design and Implementation </h1>
+            </div>
+            <footer>
+                <ul class="footer_links">
+                    <li>
+                        <p class="home">Home</p>
+                        <a class="logo" href="#">Paladin Productions <i>&copy; 2014 </i></a>
+                    </il>
+                    <li>
+                        <p class="services">Services</p>
+                        <ul>
+                            <li><a href="#">Composition</a></li>
+                            <li><a href="#">Sound Design</a></li>
+                            <li><a href="#">Video Game Audio</a></li>
+                            <li><a href="#">Audio Outsourcing </a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="contact">Contact</p>
+                        <ul>
+                            <li><a href="#">Email</a></il>
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">Facebook</a></li>
+                            <li>PHONE: (678)-939-4911</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="clients">Clients</p>
 
-		<body>
-
-			<div id="main">
-
-				<h1>A Step Closer to Audio Design and Implementation </h1>
-
-			</div>
-
-			<footer>
-
-				<ul>
-					<il>
-						<p class="home">Home</p>
-						<a class="logo" href="#">Paladin Productions <i>&copy; 2014 </i></a>
-					</il>
-					<li>
-						<p class="services">Services</p>
-
-						<ul>
-							<li><a href="#">Composition</a></li>
-							<li><a href="#">Sound Design</a></li>
-							<li><a href="#">Video Game Audio</a></li>
-							<li><a href="#">Audio Outsourcing </a></li>
-						</ul>
-					</li>
-					<li>
-						<p class="contact">Contact</p>
-
-						<ul>
-							<il><a href="#">Email</a></il>
-							<li><a href="#">Twitter</a></li>
-							<li><a href="#">Facebook</a></li>
-							<li>(678)-939-4911</li>
-						</ul>
-					</li>
-					<li>
-						<p class="clients">Clients</p>
-
-						<ul>
-							<li><a href="#">FAQ</a></li>
-						</ul>
-					</li>
-				</ul>
-			</footer>
+                        <ul>
+                            <li><a href="#">FAQ</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </footer>
 
 
 
 
 
-		</body>
+        </body>
 
 </html>
 
--- tabs.php
+++ tabs.php
@@ -1,38 +1,37 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<link rel="stylesheet" type="text/css" href="style.css" />
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-<script type="text/javascript" src="script.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
 
 </head>
 
 <div id="rounded">
 
-<img src="img/top_bg.gif"/><!-- image with rounded left and right cornors -->
-<div id="main" class="container"><!-- our main container element -->
+    <img src="img/top_bg.gif"/><!-- image with rounded left and right cornors -->
+    <div id="main" class="container"><!-- our main container element -->
 
-<h1> Erik Ashley</h1> <!-- titles-->
-<h2> Video Game Audio</h2>
+    <h1> Erik Ashley</h1> <!-- titles-->
+    <h2> Video Game Audio</h2>
 
-<ul id="navigation"> <!-- the navigation menu-->
-<li><a href="#page1">Artist Statement </a></li><!-- a few navigation buttons-->
-<li><a href="page2">Demo Reel</a></li>
-<li><a href="page3">Biography</a></li>
-<li><a href="page4">Credits</a></li>
-<li><img id="loading" src="img/ajax_load.gif" alt="loading" /></li><!-- rotating loading gif-->
-</ul>
+    <ul id="navigation"> <!-- the navigation menu-->
+        <li class="blue_button"><a href="#page1">Artist Statement </a></li><!-- a few navigation buttons-->
+        <li class="blue_button"><a href="#page2">Demo Reel</a></li>
+        <li class="blue_button"><a href="#page3">Biography</a></li>
+        <li class="blue_button"><a href="#page4">Credits</a></li>
+        <li class="blue_button"><img id="loading" src="img/ajax_load.gif" alt="loading" /></li><!-- rotating loading gif-->
+    </ul>
+    <a href="#main"></a>
+    <div class="clear"></div><!-- the above links are floated we have to use the cleardix hack-->
 
-<div class="clear"></div><!-- the above links are floated we have to use the cleardix hack-->
-
-<div id="pagecontent"><!-- this is where our AJAX-ed content goes-->
-Hello this is the default content
+    <div id="pagecontent"><!-- this is where our AJAX-ed content goes-->
+    
+</div>
 </div>
 
-</div>
+    <div class="clear"></div> 
 
-<div class="clear"></div> 
-
-<img src="img/bottom_bg.gif" /><!--the bottom two rounded cornors of the page -->
+    <img src="img/bottom_bg.gif" /><!--the bottom two rounded cornors of the page -->
 
 </div>
