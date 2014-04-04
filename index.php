192dd26c9f381fc0c41607bf369780016ef0aba6
Erik Ashley <eashley415@gmail.com>
2014-04-04 18:19:19 -0400

    update

--- index.php
+++ index.php
@@ -19,6 +19,45 @@
 
 			</div>
 
+			<footer>
+
+				<ul>
+					<il>
+						<p class="home">Home</p>
+						<a class="logo" href="#">Paladin Productions <i>&copy; 2014 </i></a>
+					</il>
+					<li>
+						<p class="services">Services</p>
+
+						<ul>
+							<li><a href="#">Composition</a></li>
+							<li><a href="#">Sound Design</a></li>
+							<li><a href="#">Video Game Audio</a></li>
+							<li><a href="#">Audio Outsourcing </a></li>
+						</ul>
+					</li>
+					<li>
+						<p class="contact">Contact</p>
+
+						<ul>
+							<il><a href="#">Email</a></il>
+							<li><a href="#">Twitter</a></li>
+							<li><a href="#">Facebook</a></li>
+							<li>(678)-939-4911</li>
+						</ul>
+					</li>
+					<li>
+						<p class="clients">Clients</p>
+
+						<ul>
+							<li><a href="#">FAQ</a></li>
+						</ul>
+					</li>
+				</ul>
+			</footer>
+
+
+
 
 
 		</body>
