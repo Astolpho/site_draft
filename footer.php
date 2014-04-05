22419a9291de883545262814aa7f3595868b740f
Erik Ashley <eashley415@gmail.com>
2014-04-04 22:49:35 -0400

    update_10

--- footer.php
+++ footer.php
@@ -1,34 +1,34 @@
-<footer >
-        <? php header('Access-Control-Allow-Origin: *');?>
-        <ul class="footer_links">
-            <li>
-                <p class="home">Home</p>
-                <a class="logo" href="#">Paladin Productions <i>&copy; 2014 </i></a>
-            </il>
-            <li>
-                <p class="services">Services</p>
-                <ul>
-                    <li><a href="#">Composition</a></li>
-                    <li><a href="#">Sound Design</a></li>
-                    <li><a href="#">Video Game Audio</a></li>
-                    <li><a href="#">Audio Outsourcing </a></li>
-                </ul>
-            </li>
-            <li>
-                <p class="contact">Contact</p>
-                <ul>
-                    <li><a href="#">Email</a></il>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Facebook</a></li>
-                    <li>PHONE: (678)-939-4911</li>
-                </ul>
-            </li>
-            <li>
-                <p class="clients">Clients</p>
+<footer>
+    <? php header('Access-Control-Allow-Origin: *');?>
+    <ul class="footer_links">
+        <li class="home_col">
+            <p class="home">Home</p>
+            <a class="logo" href="#">Paladin Productions <i>&copy; 2014 </i></a>
+        </il>
+        <li class="services_col">
+            <p class="services">Services</p>
+            <ul class="services_links">
+                <li><a href="#">Composition</a></li>
+                <li><a href="#">Sound Design</a></li>
+                <li><a href="#">Video Game Audio</a></li>
+                <li><a href="#">Audio Outsourcing </a></li>
+            </ul>
+        </li>
+        <li class="contact_col">
+            <p class="contact">Contact</p>
+            <ul class="contact_links">
+                <li><a href="#">Email</a></il>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Facebook</a></li>
+                <li>PHONE: (678)-939-4911</li>
+            </ul>
+        </li>
+        <li class "clients_col">
+            <p class="clients">Clients</p>
 
-                <ul>
-                    <li><a href="#">FAQ</a></li>
-                </ul>
-            </li>
-        </ul>
-    </footer>
+            <ul class "clients_links">
+                <li><a href="#">FAQ</a></li>
+            </ul>
+        </li>
+    </ul>
+</footer>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <? php header('Access-Control-Allow-Origin: *');?>
+    <?php header('Access-Control-Allow-Origin: *');?>
     <link rel="stylesheet" type="text/css" href="style.css" />
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
     <script type="text/javascript" src="script.js"></script>
@@ -35,7 +35,7 @@
         <div class="clear"></div> 
     </div>
 </div>
-<? php include("footer.php");?>);
+<?php include("footer.php");?>);
 </body>
 
 </html>
