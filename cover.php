26415432a8978c17dc2df83e84f40a173e28bf3b
Erik Ashley <eashley415@gmail.com>
2014-05-14 21:37:39 -0400

    bootstrap_update_1
    Cover page started.

---
+++ cover.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--<link rel="shortcut icon" href="../../assets/ico/favicon.ico">-->
+
+    <title>Erik Ashley</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/cover_bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+  <header class="container pull-left">
+        <div class="row">
+          <div class="span9">
+            <!-- Logo-->
+            <div class="logo">
+              <h1>Erik Ashley</h1>
+              <h2>Video Game Music &amp; Audio <span class="bullet">&#149;</span> <span class="gold">Atlanta, GA</span></h2>
+            </div>
+          </div>
+        </div>
+          <div class="span3">
+            <h4>Phone</h4>
+            <p>
+              Call me anytime: <br><a href="tel://678-939-4911">678.939.4911</a>
+            </p>
+
+          </div>
+          <div class="span3">
+            <h4>Email</h4>
+            <p>
+              Questions &amp; Inquiries: <br><a href="mailto:eashley@berklee.edu">eashley@berklee.edu</a>
+              
+            </p>
+          </div>
+          <div class="span3">
+            <h4>Social</h4>
+            <p>
+              
+            </p>
+          </div>
+        </div>
+      </header>
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+  </body>
+</html>
